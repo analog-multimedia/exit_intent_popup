5718631529d1915fd4fea33493c85a509977a0ac
Boris Schauer <me@bschauer.de>
2017-12-13 11:42:11 +0100

    [TASK] Updates for 8LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'Exit intent Popup',
     'description' => 'Display an Popup on exit intent',
     'category' => 'templates',
-    'version' => '1.1.1',
+    'version' => '1.2.0',
     'state' => 'beta',
     'clearcacheonload' => 1,
     'author' => 'analog digitalagentur',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'analog.de',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.12-7.99.99'
+            'typo3' => '6.2.12-8.99.99'
         ),
     ),
 );
